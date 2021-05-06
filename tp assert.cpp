7b2317482306afc1fae0a24a4b91045fc2475aa0
Maunus-dev <83046144+Maunus-dev@users.noreply.github.com>
2021-05-06 01:05:51 -0300

    Add files via upload

---
+++ tp assert.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cassert>
+using namespace std;
+
+int main(void) {
+    int entero =15;
+    assert (entero == 15) ;
+    float flotante =10,45;
+    double mayor =16,335;
+    char letra 'a';
+    
+    assert (flotante ==10,45) ;
+    assert (mayor ==16,335) ;
+    assert (letra ==a) ;
+
+
+
+
+    return 0;
+}
